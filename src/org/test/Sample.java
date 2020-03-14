101bbc230a9758464be45d7aec3940cf2fe78daa
mohanakrishnan123 <mkn5151@gmail.com>
2020-03-14 14:10:08 +0530

    code done byB1

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -3,7 +3,8 @@ package org.test;
 public class Sample {
 public static void main(String[] args) {
 	System.out.println("1st code");
-	
 	System.out.println("2nd code done by branch1");
+	System.out.println("branch1 done code for 2nd time");
+	
 }
 }
